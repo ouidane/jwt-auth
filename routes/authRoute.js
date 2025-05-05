f4abd3bb9c5878387667e40189b9cf9bf75ca451
ouidane <ouidanezakaria@gmail.com>
2025-05-05 02:19:06 +0100

    add files

---
+++ routes/authRoute.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { login, dashboard } = require("../controllers/auth");
+const authMiddleware = require("../middleware/auth");
+
+const router = express.Router();
+
+router.route("/dashboard").get(authMiddleware, dashboard);
+router.route("/login").post(login);
+
+module.exports = router;
